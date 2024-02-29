c33d0055d114e4376a93aa4bead75de82489ba39
ngarc <nicolas.garcia.presta@ensup.eu>
2024-02-29 15:12:57 +0100

    Méthode pour sauter des lignes.

--- Program.cs
+++ Program.cs
@@ -13,63 +13,71 @@ namespace NombreCache
 {
     class Program
     {
-
-        static int saisie(string message)
+        // Procédure pour sauter lignes
+        static void SauterLignes()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+        static int Saisie(string message)
         {
             int nombre;
             do
             {
-                Console.Write(message);//Changement du mot essai par le mot nombre
+                Console.Write(message);
                 nombre = int.Parse(Console.ReadLine());
             } while (nombre < 0 || nombre > 100);
             return nombre;
         }
+
         static void Main(string[] args)
         {
             int valeur, essai, nbre = 1;
-            int borneHaute = 100, borneBasse=0 ;
+            int borneHaute = 100, borneBasse = 0;
             int difference;
             int moitieDifference;
 
-            //Contrôle de la valeur saisie pour le nombre caché
-            valeur = saisie("Entrer le nombre caché (entre 0 et 100) : ");
+            valeur = Saisie("Entrer le nombre caché (entre 0 et 100) : ");
             Console.Clear();
             Console.WriteLine("Pour deviner le nombre caché, utilisez la méthode de dichotomie.");
             Console.WriteLine("https://fr.wikipedia.org/wiki/Méthode_de_dichotomie");
-            //Saisie du premier essai et contrôle
-            essai = saisie("Entrer un essai (entre 0 et 100) : ");
+            SauterLignes();
+
+            essai = Saisie("Entrer un essai (entre 0 et 100) : ");
 
             while (essai != valeur)
             {
                 if (essai > valeur)
                 {
                     borneHaute = essai;
-                    Console.WriteLine(essai + " est plus grand que le nombre caché, essayez entre la moitié de "+borneBasse+" et "+borneHaute+".");
-                    difference = borneHaute - borneBasse;
-                    moitieDifference = difference/2;
-                    Console.WriteLine("Essayez " + (borneBasse + moitieDifference));
+                    Console.WriteLine(essai +
+                                      " est plus grand que le nombre caché, essayez entre la moitié de "
+                                      + borneBasse + " et "
+                                      + borneHaute + ".");
                 }
                 else
                 {
                     borneBasse = essai;
                     Console.WriteLine(essai + " est plus petit que le nombre caché.");
-                    difference = borneHaute - borneBasse;
-                    moitieDifference = difference / 2;
-                    Console.WriteLine("Essayez " + (borneBasse + moitieDifference));
                 }
 
-                essai = saisie("Entrer un essai (entre 0 et 100) : ");
+                difference = borneHaute - borneBasse;
+                moitieDifference = difference / 2;
+                Console.WriteLine("Essayez " + (borneBasse + moitieDifference));
+
+                essai = Saisie("Entrer un essai (entre 0 et 100) : ");
 
                 nbre += 1;
             }
+
             Console.Clear();
-            Console.WriteLine("Bravo, vous avez trouvé en " + nbre + " fois !");
+            Console.WriteLine("Bravo, vous avez trouvé en "
+                              + nbre
+                              + " fois mais que seriez-vous sans l'aide du programme ?");
 
             Console.ReadLine();
         }
     }
 }
-
-
-
-
